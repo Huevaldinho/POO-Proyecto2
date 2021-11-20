7f2daad6cf30d151c3ab92f5ac525c79a116bb80
Felipe <felipeobando@estudiantec.cr>
2021-11-19 18:04:31 -0600

    Info de agregar platillo

--- src/AppAdministrador/vista/AgregarPlatilloAdmin.java
+++ src/AppAdministrador/vista/AgregarPlatilloAdmin.java
@@ -4,8 +4,25 @@ import javax.swing.*;
 
 public class AgregarPlatilloAdmin extends JFrame {
     private JPanel panel;
-    private JTextField textoNombrePlatillo;
+    private JTextField txtNombrePlatillo;
     private JComboBox comboboxTipoPlatillo;
+    private JTextField txtDescripcion;
+    private JLabel lblDescripcion;
+    private JLabel lblTamano;
+    private JTextField txtTamanno;
+    private JLabel lblPiezasXPorcion;
+    private JTextField txtPiezasXPorcion;
+    private JLabel lblCaloriasXPorcion;
+    private JTextField txtCaloriasXPorcion;
+    private JLabel lblTipoDePlatillo;
+    private JLabel lblNombrePlatillo;
+    private JLabel lblCaloriasXPiezas;
+    private JTextField txtCaloriasXPieza;
+    private JLabel lblPrecio;
+    private JTextField txtPrecio;
+    private JLabel lblImagen;
+    private JLabel txtInsertarImagen;
+    private JButton btnAceptar;
 
     public AgregarPlatilloAdmin() {
         super("Agregar Platillo");
